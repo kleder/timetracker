3ce208559a8e131504fed7099005c49721dcbfbd
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2017-12-29 15:37:37 +0100

    edit boards view

--- src/app/components/edit-board/edit-board.component.ts
+++ src/app/components/edit-board/edit-board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { DatabaseService } from '../../services/database.service'
+import { ToasterService } from '../../services/toaster.service'
 
 @Component({
   selector: 'app-edit-board',
@@ -11,9 +13,12 @@ export class EditBoardComponent implements OnInit {
   public accountId: number
   public accountName: string
   public accountUrl: string
+  public boardStates: any
   constructor(
     public activatedRoute: ActivatedRoute,
-    public router: Router
+    public router: Router,
+    public databaseService: DatabaseService,
+    public toasterService: ToasterService
   ) { }
 
   ngOnInit() {
@@ -25,6 +30,34 @@ export class EditBoardComponent implements OnInit {
       this.accountName = params['accountName']
       this.accountUrl = params['accountUrl']
     });
+    console.log("this.boardName", this.boardName)
+    console.log("this.accountId", this.accountId)
+    this.getBoardStates(this.accountId, this.boardName)
+  }
+
+  getBoardStates(accountId, boardName) {
+    this.databaseService.getBoardStates(accountId, boardName).then(data => {
+      console.log("data", data)
+      this.boardStates = data
+    })
+  }
+
+  updateStatesColors(boardStates) {
+    for (let i = 0; i < boardStates.length; i++) {
+      if (!/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(boardStates[i].hexColor)) {
+        this.toasterService.showToaster('It isn\'t hex format!', 'error')          
+        return false        
+      }
+    }
+    boardStates.forEach((state) => {
+      this.databaseService.changeColorStates(state.accountId, state.boardName, state.state, state.hexColor).then(data => {
+        this.toasterService.showToaster('Color has been changed!', 'success')      
+      })
+    })
+  }
+
+  changeBackground(bgColor) {
+    return { 'background-color': bgColor };
   }
 
   goBack() {
