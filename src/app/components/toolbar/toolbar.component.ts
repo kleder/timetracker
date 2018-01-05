8c186cd297e6ff495fb98f294c3c2cec796cef60
Katarzyna Sandej <sandejka@gmail.com>
2018-01-05 11:26:44 +0100

    reset changes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { remote } from 'electron'
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -7,22 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   public isMac: boolean = (process.platform == 'darwin') ? true : false    
+  private win;
   constructor() {
+    this.win = remote.getCurrentWindow();
   }
 
   ngOnInit() {
-  }
+      }
 
   closeApp() {
-     
+      //electron.remote.app.quit()
+      this.win.close();
+      console.log("close")
   }
 
   minimizeApp() {
-
+    
+    this.win.minimize();
   }
 
   fullScreen() {
-
+    
+    this.win.maximize();
   }
 
 }
