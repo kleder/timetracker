ed07531fee3d9ad5e708c7670ca867e77e6f99a6
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2018-01-19 15:23:58 +0100

    fix jumping loader

--- src/app/components/tracking/dashboard/dashboard.component.ts
+++ src/app/components/tracking/dashboard/dashboard.component.ts
@@ -71,7 +71,6 @@ export class DashboardComponent implements OnInit {
   public init() {
     this.api.getAllAgiles().then(
       data => {
-        this.httpService.loader = false
         this.agiles = data
         this.agiles.forEach(agile => {
           this.getAgileVisibility(agile.name)
