a46bc7791a1fd3f01b86fefc7814e5b8b7f979e0
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2018-01-22 10:51:18 +0100

    add loading spinner, css changes

--- src/app/components/edit-account/edit-account.component.ts
+++ src/app/components/edit-account/edit-account.component.ts
@@ -22,7 +22,7 @@ export class EditAccountComponent implements OnInit {
     public databaseService: DatabaseService,
     public activatedRoute: ActivatedRoute,
     public api: ApiService,
-    public http: HttpService,
+    public httpService: HttpService,
     public router: Router,
     public account: AccountService,
     public toasterService: ToasterService
@@ -45,7 +45,7 @@ export class EditAccountComponent implements OnInit {
   public getAllAgiles() {
     this.api.getAllAgiles().then(
       data => {
-        this.http.loader = false
+        this.httpService.loader = false
         this.agiles = data
         this.agiles.forEach(agile => {
           this.getAgileVisibility(agile.name)          
