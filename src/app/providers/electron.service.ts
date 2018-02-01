967d9e56b8f678ef19624de2071ff5108b6b89fd
Kamil Dobrzynski <kamil.dobrzynski@kleder.co>
2018-02-01 14:37:18 +0100

    clean up

--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 
-// If you import a module but never use any of the imported values other than as TypeScript types,
-// the resulting javascript file will look as if you never imported the module at all.
 import { ipcRenderer } from 'electron';
 import * as childProcess from 'child_process';
 
@@ -12,7 +10,6 @@ export class ElectronService {
   childProcess: typeof childProcess;
 
   constructor() {
-    // Conditional imports
     if (this.isElectron()) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
